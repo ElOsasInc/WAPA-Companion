203d4a0335e8a6ebc90c403cf33dc6cb6f324663
RubenGJ2005 <rubengjuarez@gmail.com>
2026-01-04 20:02:54 -0600

    pantalla asistencia con nombre bueno

---
+++ app/src/main/java/com/example/wapacompanion/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.wapacompanion
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.wapacompanion.ui.screens.asistenciaScreen
+import androidx.core.content.ContextCompat
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import com.example.wapacompanion.ui.theme.WapacompanionTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Verificar permiso
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+            != PackageManager.PERMISSION_GRANTED) {
+
+            // Solicitar permiso
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 0)
+        }
+        setContent {
+            WapacompanionTheme {
+                val navController = rememberNavController()
+
+                NavHost(
+                    navController = navController,
+                    startDestination = "asistencia"
+                ) {
+                    composable("asistencia") {
+                        asistenciaScreen()
+                    }
+                }
+            }
+        }
+    }
+}
